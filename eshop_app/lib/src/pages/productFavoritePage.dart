acf5b37b1e6c864cdaed1e5acf410214c5508e89
Ramjan75 <ramjanplus5@gmail.com>
2021-07-23 01:10:52 +0600

    first commit

--- eshop_app/lib/src/pages/productFavoritePage.dart
+++ eshop_app/lib/src/pages/productFavoritePage.dart
@@ -35,81 +35,82 @@ class ProductFavoritePage extends StatelessWidget {
       ),
       body: favoriteBox.isEmpty
           ? Center(child: Text('No Favorite Products'))
-          : Container(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 2,
-                        crossAxisSpacing: 2,
-                        childAspectRatio: .60,
-                      ),
-                      shrinkWrap: true,
-                      primary: false,
-                      itemCount: favoriteBox.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        final item = favoriteBox.getAt(index);
+          : ValueListenableBuilder(
+              builder: (BuildContext context, value, Widget? child) {
+                return Container(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 2,
+                            crossAxisSpacing: 2,
+                            childAspectRatio: .60,
+                          ),
+                          shrinkWrap: true,
+                          primary: false,
+                          itemCount: favoriteBox.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final item = favoriteBox.getAt(index);
 
-                        return Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Container(
-                            height: 30,
-                            width: 30,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            child: Stack(
-                              alignment: Alignment.center,
-                              clipBehavior: Clip.none,
-                              children: [
-                                CircleAvatar(
-                                  backgroundColor:
-                                      Colors.blue.shade100.withOpacity(.80),
-                                  radius: 65,
+                            return Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Container(
+                                height: 30,
+                                width: 30,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(30),
                                 ),
-                                CircleAvatar(
-                                  backgroundColor: Colors.white,
-                                  radius: 58,
-                                ),
-                                CircleAvatar(
-                                  backgroundColor:
-                                      Colors.blue.shade100.withOpacity(.80),
-                                  radius: 56,
-                                ),
-                                Positioned(
-                                  top: 12,
-                                  left: 12,
-                                  child: Container(
-                                    padding: EdgeInsets.all(4),
-                                    height: 22,
-                                    width: 42,
-                                    decoration: BoxDecoration(
-                                        color: Colors.blue.shade100
-                                            .withOpacity(.80),
-                                        borderRadius:
-                                            BorderRadius.circular(18)),
-                                    child: Center(
-                                      child: KText(
-                                        text: '${item!.discount}%',
-                                        color: Colors.black54,
-                                        fontSize: 13,
+                                child: Stack(
+                                  alignment: Alignment.center,
+                                  clipBehavior: Clip.none,
+                                  children: [
+                                    CircleAvatar(
+                                      backgroundColor:
+                                          Colors.blue.shade100.withOpacity(.80),
+                                      radius: 65,
+                                    ),
+                                    CircleAvatar(
+                                      backgroundColor: Colors.white,
+                                      radius: 58,
+                                    ),
+                                    CircleAvatar(
+                                      backgroundColor:
+                                          Colors.blue.shade100.withOpacity(.80),
+                                      radius: 56,
+                                    ),
+                                    Positioned(
+                                      top: 12,
+                                      left: 12,
+                                      child: Container(
+                                        padding: EdgeInsets.all(4),
+                                        height: 22,
+                                        width: 42,
+                                        decoration: BoxDecoration(
+                                            color: Colors.blue.shade100
+                                                .withOpacity(.80),
+                                            borderRadius:
+                                                BorderRadius.circular(18)),
+                                        child: Center(
+                                          child: KText(
+                                            text: '${item!.discount}%',
+                                            color: Colors.black54,
+                                            fontSize: 13,
+                                          ),
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                ),
-                                Positioned(
-                                  top: 12,
-                                  right: 12,
-                                  child: GestureDetector(
-                                    onTap: () => favoriteBox.delete(index),
-                                    child: ValueListenableBuilder(
-                                      builder: (BuildContext context, value,
-                                          Widget? child) {
-                                        return Container(
+                                    Positioned(
+                                      top: 12,
+                                      right: 12,
+                                      child: GestureDetector(
+                                        onTap: () =>
+                                            favoriteBox.delete(item.id),
+                                        child: Container(
                                           padding: EdgeInsets.all(4),
                                           decoration: BoxDecoration(
                                               color: favoriteBox
@@ -126,72 +127,74 @@ class ProductFavoritePage extends StatelessWidget {
                                               color: Colors.white,
                                             ),
                                           ),
-                                        );
-                                      },
-                                      valueListenable: favoriteBox.listenable(),
+                                        ),
+                                      ),
                                     ),
-                                  ),
-                                ),
-                                Positioned(
-                                    bottom: 20,
-                                    child: Column(
-                                      children: [
-                                        KText(
-                                          text: '${item.title}',
-                                          fontFamily: 'Lato Regular',
-                                          fontSize: 13,
-                                          color: Colors.black,
-                                        ),
-                                        SizedBox(
-                                          height: 10,
-                                        ),
-                                        KText(
-                                          text: '\$${item.price}',
-                                          fontFamily: 'Lato Bold',
-                                          fontSize: 14,
-                                          color: Colors.black,
-                                        ),
-                                        SizedBox(
-                                          height: 10,
-                                        ),
-                                        Row(
+                                    Positioned(
+                                        bottom: 20,
+                                        child: Column(
                                           children: [
-                                            RatingBar.builder(
-                                              itemSize: 12,
-                                              initialRating: double.parse(
-                                                  '${item.rating}'),
-                                              direction: Axis.horizontal,
-                                              allowHalfRating: true,
-                                              itemCount: 5,
-                                              itemPadding: EdgeInsets.symmetric(
-                                                  horizontal: 4.0),
-                                              itemBuilder: (context, _) => Icon(
-                                                Icons.star,
-                                                color: Colors.amber,
-                                              ),
-                                              onRatingUpdate: (rating) {
-                                                print(rating);
-                                              },
+                                            KText(
+                                              text: '${item.title}',
+                                              fontFamily: 'Lato Regular',
+                                              fontSize: 13,
+                                              color: Colors.black,
                                             ),
-                                            Text(
-                                              '${item.rating})',
-                                              style: TextStyle(
-                                                fontSize: 12,
-                                              ),
+                                            SizedBox(
+                                              height: 10,
                                             ),
+                                            KText(
+                                              text: '\$${item.price}',
+                                              fontFamily: 'Lato Bold',
+                                              fontSize: 14,
+                                              color: Colors.black,
+                                            ),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Row(
+                                              children: [
+                                                RatingBar.builder(
+                                                  itemSize: 12,
+                                                  initialRating: double.parse(
+                                                      '${item.rating}'),
+                                                  direction: Axis.horizontal,
+                                                  allowHalfRating: true,
+                                                  itemCount: 5,
+                                                  itemPadding:
+                                                      EdgeInsets.symmetric(
+                                                          horizontal: 4.0),
+                                                  itemBuilder: (context, _) =>
+                                                      Icon(
+                                                    Icons.star,
+                                                    color: Colors.amber,
+                                                  ),
+                                                  onRatingUpdate: (rating) {
+                                                    print(rating);
+                                                  },
+                                                ),
+                                                Text(
+                                                  '${item.rating})',
+                                                  style: TextStyle(
+                                                    fontSize: 12,
+                                                  ),
+                                                ),
+                                              ],
+                                            )
                                           ],
-                                        )
-                                      ],
-                                    ))
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    ),
+                                        ))
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                );
+              },
+              valueListenable: favoriteBox.listenable(),
             ),
     );
   }
